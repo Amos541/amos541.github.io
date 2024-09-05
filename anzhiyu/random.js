175771630af84cc9770f57e3ce05ebb53f3b8063
yun2060 <2060103908@qq.com>
2024-09-05 17:02:44 +0800

    Site updated: 2024-09-05 17:02:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["Development/搭建图床/","Hexo/Algolia搜索配置/","Hexo/hello-world/","Hexo/使用hexo命令创建新文章/","Hexo/shoka主题介绍/","Hexo/将shoka主题中valine评论系统更换为waline系统/","Hexo/更换主题/","Knowledge/markdown语法/","Knowledge/数据结构初识/","Knowledge/数据结构学习（一）/","Others/便携版vscode修改数据路径/","Others/关于如何解决github直连失败的方法/","Others/提取wallpaper壁纸/","Others/终端美化篇/","Problems/hexo的图片插入/"];function toRandomPost(){
+var posts=["Development/搭建图床/","Hexo/Algolia搜索配置/","Hexo/hello-world/","Hexo/shoka主题介绍/","Hexo/使用hexo命令创建新文章/","Hexo/将shoka主题中valine评论系统更换为waline系统/","Hexo/更换主题/","Knowledge/markdown语法/","Knowledge/数据结构初识/","Knowledge/数据结构学习（一）/","Knowledge/数据结构学习（二）/","Others/便携版vscode修改数据路径/","Others/关于如何解决github直连失败的方法/","Others/提取wallpaper壁纸/","Others/终端美化篇/","Problems/hexo的图片插入/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
